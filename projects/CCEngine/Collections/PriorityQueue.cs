016088d5ff28127529caeca03f7b68345c6ef64e
askeladdk <askeladdk@users.noreply.github.com>
2017-06-27 19:26:27 +0200

    Cleaned up the priority queue code.

--- projects/CCEngine/Collections/PriorityQueue.cs
+++ projects/CCEngine/Collections/PriorityQueue.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace CCEngine.Collections
 {
 	public sealed class PriorityQueue<TKey, TValue>
-		: IEnumerable, IEnumerable<KeyValuePair<TKey, TValue>>
 	{
 		private readonly List<KeyValuePair<TKey, TValue>> heap = new List<KeyValuePair<TKey, TValue>>();
 		private readonly IComparer<TKey> comparer;
 
-		/// <returns></returns>
 		private int Compare(int idx0, int idx1)
 		{
 			return comparer.Compare(heap[idx0].Key, heap[idx1].Key);
@@ -35,7 +32,7 @@ namespace CCEngine.Collections
 
 		private void BubbleDown(int idx)
 		{
-			var count = heap.Count;
+			var count = this.Count;
 			var last = (count - 2) / 2;
 
 			while (idx <= last)
@@ -57,35 +54,6 @@ namespace CCEngine.Collections
 			this.comparer = comparer;
 		}
 
-		public void Enqueue(KeyValuePair<TKey, TValue> item)
-		{
-			heap.Add(item);
-			var last = heap.Count - 1;
-			BubbleUp(last);
-		}
-
-		public void Enqueue(TKey priority, TValue value)
-		{
-			Enqueue(new KeyValuePair<TKey, TValue>(priority, value));
-		}
-
-		public TValue Dequeue()
-		{
-			if (heap.Count == 0)
-				throw new InvalidOperationException("PriorityQueue is empty");
-			var value = heap[0].Value;
-			var last = heap.Count - 1;
-			heap[0] = heap[last];
-			heap.RemoveAt(last);
-			BubbleDown(0);
-			return value;
-		}
-
-		public void Clear()
-		{
-			heap.Clear();
-		}
-
 		public int Count
 		{
 			get { return heap.Count; }
@@ -93,26 +61,38 @@ namespace CCEngine.Collections
 
 		public TValue Peek()
 		{
-			if (heap.Count == 0)
+			if (this.Count == 0)
 				throw new InvalidOperationException("PriorityQueue is empty");
 			return heap[0].Value;
 		}
 
+		public void Enqueue(TKey priority, TValue value)
+		{
+			heap.Add(new KeyValuePair<TKey, TValue>(priority, value));
+			var last = this.Count - 1;
+			BubbleUp(last);
+		}
+
+		public TValue Dequeue()
+		{
+			var value = Peek();
+			var last = this.Count - 1;
+			heap[0] = heap[last];
+			heap.RemoveAt(last);
+			BubbleDown(0);
+			return value;
+		}
+
 		public bool TryDequeue(out TValue value)
 		{
-			var ok = heap.Count > 0;
+			var ok = this.Count > 0;
 			value = ok ? Dequeue() : default(TValue);
 			return ok;
 		}
 
-		IEnumerator IEnumerable.GetEnumerator()
+		public void Clear()
 		{
-			return heap.GetEnumerator();
-		}
-
-		IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator()
-		{
-			return heap.GetEnumerator();
+			heap.Clear();
 		}
 	}
 
