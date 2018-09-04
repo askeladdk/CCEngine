2cdd2fafb8c00684e8cffa249b1cc2326dc84b98
askeladdk <karel.vd.veldt@gmail.com>
2018-09-04 16:38:19 +0200

    Refactored OrderedDictionary

--- projects/CCEngine/Collections/OrderedDictionary.cs
+++ projects/CCEngine/Collections/OrderedDictionary.cs
@@ -8,14 +8,23 @@ using System.Linq;
 namespace CCEngine.Collections
 {
 	/// <summary>
-	/// Generic implementation of IOrderedDictionary.
+	/// Represents a generic indexed collection of key/value pairs.
 	/// </summary>
-	/// <typeparam name="TKey">Key type.</typeparam>
-	/// <typeparam name="TValue">Value type.</typeparam>
+	public interface IOrderedDictionary<TKey, TValue> : IDictionary<TKey, TValue>
+	{
+		int IndexOf(TKey key);
+		void Insert(int index, TKey key, TValue value);
+		KeyValuePair<TKey, TValue> GetAt(int index);
+		void SetAt(int index, KeyValuePair<TKey, TValue> value);
+	}
+
+	/// <summary>
+	/// Generic indexed collection of key/value pairs.
+	/// </summary>
 	public class OrderedDictionary<TKey, TValue> :
 		KeyedCollection<TKey, KeyValuePair<TKey, TValue>>,
-		IDictionary<TKey, TValue>,
-		IOrderedDictionary
+		IOrderedDictionary<TKey, TValue>,
+		IReadOnlyDictionary<TKey, TValue>
 	{
 		protected override TKey GetKeyForItem(KeyValuePair<TKey, TValue> item)
 		{
@@ -26,206 +35,63 @@ namespace CCEngine.Collections
 			: base()
 		{ }
 
-		/// <summary>
-		/// Set item by key.
-		/// </summary>
-		/// <param name="key"></param>
-		/// <param name="value"></param>
-		public void Set(TKey key, TValue value)
-		{
-			Add(key, value);
-		}
+		#region IOrderedDictionary<TKey, TValue>
 
-		/// <summary>
-		/// Get item by key.
-		/// </summary>
-		/// <param name="key"></param>
-		/// <returns></returns>
-		public TValue Get(TKey key)
+		public int IndexOf(TKey key)
 		{
-			return base[key].Value;
-		}
-
-		/// <summary>
-		/// Set item by index.
-		/// </summary>
-		/// <param name="index"></param>
-		/// <param name="value"></param>
-		public void SetAt(int index, TValue value)
-		{
-			var kv = base[index];
-			base[index] = new KeyValuePair<TKey, TValue>(kv.Key, value);
-		}
-
-		/// <summary>
-		/// Get item by index.
-		/// </summary>
-		/// <param name="index"></param>
-		/// <returns></returns>
-		public TValue GetAt(int index)
-		{
-			return base[index].Value;
-		}
-
-		public void Add(TKey key, TValue value)
-		{
-			var kv = new KeyValuePair<TKey, TValue>(key, value);
 			if(base.Contains(key))
-			{
-				int index = base.IndexOf(base[key]);
-				base[index] = kv;
-			}
-			else
-			{
-				base.Add(kv);
-			}
+				return base.IndexOf(base[key]);
+			return -1;
 		}
 
-		public new TValue this[TKey key]
+		public KeyValuePair<TKey, TValue> GetAt(int index)
 		{
-			get
-			{
-				return base[key].Value;
-			}
-
-			set
-			{
-				Add(key, value);
-			}
+			return base[index];
 		}
 
-		public new TValue this[int index]
+		public void SetAt(int index, KeyValuePair<TKey, TValue> item)
 		{
-			get
-			{
-				return base[index].Value;
-			}
-
-			set
-			{
-				var kv = base[index];
-				base[index] = new KeyValuePair<TKey, TValue>(kv.Key, value);
-			}
+			base[index] = item;
 		}
 
-		#region IOrderedDictionary
-
-		IDictionaryEnumerator IOrderedDictionary.GetEnumerator()
+		public void Insert(int index, TKey key, TValue value)
 		{
-			return (IDictionaryEnumerator)this.GetEnumerator();
-		}
-
-		void IOrderedDictionary.Insert(int index, object key, object value)
-		{
-			base.Insert(index, new KeyValuePair<TKey, TValue>((TKey)key, (TValue)value));
-		}
-
-		object IOrderedDictionary.this[int index]
-		{
-			get
-			{
-				return base[index].Value;
-			}
-
-			set
-			{
-				var kv = base[index];
-				base[index] = new KeyValuePair<TKey, TValue>(kv.Key, (TValue)value);
-			}
-		}
-
-		#endregion
-
-		#region IDictionary
-
-		void IDictionary.Add(object key, object value)
-		{
-			this.Add((TKey)key, (TValue)value);
-		}
-
-		void IDictionary.Remove(object key)
-		{
-			Remove((TKey)key);
-		}
-
-		bool IDictionary.Contains(object key)
-		{
-			return Contains((TKey)key);
-		}
-
-		IDictionaryEnumerator IDictionary.GetEnumerator()
-		{
-			return (IDictionaryEnumerator)this.GetEnumerator();
-		}
-
-		bool IDictionary.IsFixedSize
-		{
-			get
-			{
-				return false;
-			}
-		}
-
-		bool IDictionary.IsReadOnly
-		{
-			get
-			{
-				return false;
-			}
-		}
-
-		ICollection IDictionary.Keys
-		{
-			get
-			{
-				return (ICollection)Dictionary.Keys;
-			}
-		}
-
-		ICollection IDictionary.Values
-		{
-			get
-			{
-				return Dictionary.Values.Select(x => x.Value).ToList();
-			}
-		}
-
-		public object this[object key]
-		{
-			get
-			{
-				return base[(TKey)key].Value;
-			}
-
-			set
-			{
-				Add((TKey)key, (TValue)value);
-			}
+			base.Insert(index, KeyValuePair.Create(key, value));
 		}
 
 		#endregion
 
 		#region IDictionary<TKey, TValue>
 
-		bool IDictionary<TKey, TValue>.ContainsKey(TKey key)
+		public new TValue this[TKey key]
 		{
-			return Dictionary.ContainsKey(key);
+			get => base[key].Value;
+			set => Add(key, value);
 		}
 
 		public ICollection<TKey> Keys
 		{
-			get
-			{
-				return Dictionary.Keys;
-			}
+			get => Dictionary.Keys.ToList();
 		}
 
 		public ICollection<TValue> Values
 		{
-			get
-			{
-				return Dictionary.Values.Select(x => x.Value).ToList();
-			}
+			get => Dictionary.Values.Select(x => x.Value).ToList();
+		}
+
+		public bool ContainsKey(TKey key)
+		{
+			return Dictionary.ContainsKey(key);
+		}
+
+		public void Add(TKey key, TValue value)
+		{
+			var kv = KeyValuePair.Create(key, value);
+			var index = IndexOf(key);
+			if(index != -1)
+				base[index] = kv;
+			else
+				base.Add(kv);
 		}
 
 		public bool TryGetValue(TKey key, out TValue value)
@@ -238,6 +104,20 @@ namespace CCEngine.Collections
 			}
 			value = default(TValue);
 			return false;
+		}
+
+		#endregion
+
+		#region IReadOnlyDictionary<TKey, TValue>
+
+		IEnumerable<TKey> IReadOnlyDictionary<TKey, TValue>.Keys
+		{
+			get => Dictionary.Keys;
+		}
+
+		IEnumerable<TValue> IReadOnlyDictionary<TKey, TValue>.Values
+		{
+			get => Dictionary.Values.Select(x => x.Value);
 		}
 
 		#endregion
