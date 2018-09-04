4a2f3b8541440564c7e857d713bc2340eda17c5c
askeladdk <karel.vd.veldt@gmail.com>
2018-09-04 17:43:06 +0200

    Fix for new Entity datatype

--- projects/CCEngine/Simulation/Mission.cs
+++ projects/CCEngine/Simulation/Mission.cs
@@ -1,3 +1,5 @@
+using CCEngine.ECS;
+
 namespace CCEngine.Simulation
 {
 	public enum MissionType
@@ -7,29 +9,29 @@ namespace CCEngine.Simulation
 
 	public interface IMission : IMessage
 	{
-		int EntityID { get; }
+		Entity Entity { get; }
 		MissionType MissionType { get; }
 	}
 
 	public class MissionMove : IMission
 	{
-		private int entityId;
+		private Entity entity;
 		private CPos destination;
 
 		public LogLevel LogLevel { get => LogLevel.Debug; }
-		public int EntityID { get => entityId; }
+		public Entity Entity { get => entity; }
 		public MissionType MissionType { get => MissionType.Move; }
 		public CPos Destination { get => destination; }
 
-		public MissionMove(int entityId, CPos destination)
+		public MissionMove(Entity entity, CPos destination)
 		{
-			this.entityId = entityId;
+			this.entity = entity;
 			this.destination = destination;
 		}
 
 		public override string ToString()
 		{
-			return "Entity {0} -> Move To {1}".F(entityId, destination);
+			return "Entity {0} -> Move To {1}".F(entity, destination);
 		}
 	}
 }
